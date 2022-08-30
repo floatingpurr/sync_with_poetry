2a2f564094a77c0312d3a8e0fea27f66b57dbc41
Andrea <floatingpurr@users.noreply.github.com>
2022-08-30 15:27:06 +0200

    Add bandit and commitizen

--- sync_with_poetry/db.py
+++ sync_with_poetry/db.py
@@ -3,12 +3,24 @@ DEPENDENCY_MAPPING = {
         "repo": "https://github.com/pre-commit/mirrors-autopep8",
         "rev": "v${rev}",
     },
+    "bandit": {
+        "repo": "https://github.com/PyCQA/bandit",
+        "rev": "${rev}",
+    },
     "black": {
         "repo": "https://github.com/psf/black",
         "rev": "${rev}",
     },
+    "commitizen": {
+        "repo": "https://github.com/commitizen-tools/commitizen",
+        "rev": "v${rev}",
+    },
     "flake8": {
         "repo": "https://github.com/pycqa/flake8",
+        "rev": "${rev}",
+    },
+    "flakeheaven": {
+        "repo": "https://github.com/flakeheaven/flakeheaven",
         "rev": "${rev}",
     },
     "isort": {
@@ -22,9 +34,5 @@ DEPENDENCY_MAPPING = {
     "pyupgrade": {
         "repo": "https://github.com/asottile/pyupgrade",
         "rev": "v${rev}",
-    },
-    "flakeheaven": {
-        "repo": "https://github.com/flakeheaven/flakeheaven",
-        "rev": "${rev}",
     },
 }
